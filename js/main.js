f7bcbcedaf35df74a00c46fa922df994b45d5ad1
dragon-v-palte <45958392+dragon-v-palte@users.noreply.github.com>
2019-09-19 13:10:30 +0500

    7

--- js/main.js
+++ js/main.js
@@ -1,6 +1,43 @@
-$(document).ready(function(){
-  $("#thanks-letter").on("click", function() {
-	$("#image-preview__content").attr('src', $("#thanks-letter").attr('src'));
-    $("#image-preview").modal();
-  });
-});
+var upButton = document.getElementById("button-up");
+
+var pushPin = document.getElementById("push-pin");
+var hero = document.getElementsByClassName("hero-block")[0];
+var menu = document.getElementById("menu");
+var sticky = menu.offsetTop;
+
+window.onscroll = function() {
+	if (document.body.scrollTop > 200 || document.documentElement.scrollTop > 200)
+		upButton.style.display = "block";
+	else
+		upButton.style.display = "none";
+	stickyMenu();
+}
+
+function stickyMenu() {
+	if (window.pageYOffset > sticky && pushPin.classList.contains("pressed")) {
+		menu.classList.add("sticky");
+		hero.style.paddingTop = "60px";
+	} else {
+		menu.classList.remove("sticky");
+		hero.style.paddingTop = 0;
+	}
+}
+
+function upFunction() {
+	scrollUp(800);
+}
+
+function scrollUp(scrollDuration) {
+    var scrollStep = -window.scrollY / (scrollDuration / 15),
+        scrollInterval = setInterval(function(){
+        if ( window.scrollY != 0 ) {
+            window.scrollBy( 0, scrollStep );
+        }
+        else clearInterval(scrollInterval); 
+    },15);
+}
+
+pushPin.onclick = function() {
+	pushPin.classList.toggle("pressed");
+	stickyMenu();
+}
